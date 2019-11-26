86b9621077a2a5d4a3b9ce6cf91d39d64cd8e57f
levine-ye <yeygaooo@163.com>
2019-11-26 16:48:50 +0800

    Send multiple emails by python

--- batch_send_exchange_email.py
+++ batch_send_exchange_email.py
@@ -1,15 +1,16 @@
 # 使用exchangelib实现Exchange邮件发送
 # 1. 使用"pip3 install exchangelib"安装exchangelib
 import datetime
-
 import os
+
 from exchangelib import DELEGATE, Account, Credentials, Message, Mailbox, Body, FileAttachment
+
 
 # real send email
 def Email(to, cc, subject, body, files):
     creds = Credentials(
         username='wei.yw.ye',
-        password='****************'
+        password='Gaochan@199008196'
     )
     # config exchange email account, to access server
     account = Account(
@@ -19,27 +20,46 @@ def Email(to, cc, subject, body, files):
         access_type=DELEGATE
     )
     # config email infomation：account、to reciever、cc reciever、subject、body
+    to_recipients = list()
+    cc_recipients = list()
+    for t in to:
+        to_recipients.append(Mailbox(email_address=t))
+    for c in cc:
+        cc_recipients.append(Mailbox(email_address=c))
+
     message = Message(
         account=account,
         subject=subject,
         body=Body(body),
-        to_recipients=[Mailbox(email_address=to)],
-        cc_recipients=[Mailbox(email_address=cc)]
+        to_recipients=to_recipients,
+        cc_recipients=cc_recipients
     )
 
     # add attachment
     for path in files:
-        with open(r"attachment/"+path, 'rb') as f:
+        with open(r"attachment/" + path, 'rb') as f:
             myfile = FileAttachment(name=path, content=f.read())
         message.attach(myfile)
 
     message.send_and_save()
 
+
 def loadFiles(dirPath):
     for root, dirs, files in os.walk(dirPath):
         return files
 
+
 if __name__ == '__main__':
+    # 发送邮件的份数和to_recievers的长度一致
+    total_to_recievers = [
+        ["1047924016@qq.com"],
+        ["yeygaooo@163.com"]
+    ]
+    total_cc_recievers = [
+        ["yeygaooo@163.com"],
+        ["1047924016@qq.com"]
+    ]
+
     dirPath = r"attachment"
     files = loadFiles(dirPath)
     # load
@@ -48,21 +68,50 @@ if __name__ == '__main__':
         temp = f.split("_")[1]
         print("temp is " + temp)
         subject_header = subject_header + temp + "&"
-    if(subject_header.endswith("&")):
+    if (subject_header.endswith("&")):
         subject_header = subject_header[:-1]
     print("subject_header is " + subject_header)
     today = datetime.date.today()
     formatted_today = today.strftime('%m')
     print("today is " + formatted_today);
-    to_reciever = "1047924016@qq.com"
-    cc_reciever = "yeygaooo@163.com"
-    split = to_reciever.split("@")
     message = "Dear All\n" \
               "\n" \
               "Please find cost center report for P01 FY20 thanks.\n" \
               "\n" \
               "Mit freundlichen Gruben\n" \
               "Chan Gao"
-    subject = "830306&830301 Cost center report P" + formatted_today + ".20"
+    subject = subject_header + " Cost center report P" + formatted_today + ".20"
     print("subject is " + subject);
-    Email(to_reciever, cc_reciever, subject, message, files)
+
+    for index, val in enumerate(total_to_recievers):
+        to_recievers = val
+        cc_recievers = total_cc_recievers[index]
+        Email(to_recievers, cc_recievers, subject, message, files)
+
+    # to_reciever = "1047924016@qq.com"  #
+    # cc_reciever = "yeygaooo@163.com"
+    #
+    # dirPath = r"attachment"
+    # files = loadFiles(dirPath)
+    # # load
+    # subject_header = ""
+    # for f in files:
+    #     temp = f.split("_")[1]
+    #     print("temp is " + temp)
+    #     subject_header = subject_header + temp + "&"
+    # if (subject_header.endswith("&")):
+    #     subject_header = subject_header[:-1]
+    # print("subject_header is " + subject_header)
+    # today = datetime.date.today()
+    # formatted_today = today.strftime('%m')
+    # print("today is " + formatted_today);
+    # split = to_reciever.split("@")
+    # message = "Dear All\n" \
+    #           "\n" \
+    #           "Please find cost center report for P01 FY20 thanks.\n" \
+    #           "\n" \
+    #           "Mit freundlichen Gruben\n" \
+    #           "Chan Gao"
+    # subject = subject_header + " Cost center report P" + formatted_today + ".20"
+    # print("subject is " + subject);
+    # Email(to_reciever, cc_reciever, subject, message, files)
